f7c16699f9b0c5030966a3dd26f49a59ba556b28
Duresa Eshetu <duresaeshetu2001@gmail.com>
2024-03-11 13:27:45 +0300

    exchange

---
+++ none/src/navigation/Sidenav.js
@@ -0,0 +1,70 @@
+import React from 'react'
+import './Sidenav.css'
+
+import HomeIcon from "@mui/icons-material/Home";
+import SearchIcon from "@mui/icons-material/Search";
+import ExploreIcon from "@mui/icons-material/Explore";
+import SlideshowIcon from "@mui/icons-material/Slideshow";
+import ChatIcon from "@mui/icons-material/Chat";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
+import MenuIcon from "@mui/icons-material/Menu";
+import { Avatar } from "@mui/material";
+
+import logo from './XY-removebg-preview.png'
+
+function Sidenav() {
+  return (
+    <div className='sidenav'>
+       <img className='sidenav__logo' src={logo} alt='logo' />
+
+       <div className='sidenav__buttons'>
+            <button className='sidenav__button'>
+                <HomeIcon />
+                <span>Home</span>
+            </button>
+
+            <button className='sidenav__button'>
+                <SearchIcon />
+                <span>Explore</span>
+            </button>
+
+            <button className='sidenav__button'>
+                <ExploreIcon />
+                <span>Explore</span>  
+            </button>
+
+            <button className='sidenav__button'>
+                <SlideshowIcon />
+                <span>Slideshow</span>    
+            </button>
+
+            <button className='sidenav__button'>
+                <ChatIcon />
+                <span>Chat</span>
+            </button>
+
+            <button className='sidenav__button'>
+                <FavoriteBorderIcon />
+                <span>Favorites</span>
+            </button>
+
+            <button className='sidenav__button'>
+                <AddCircleOutlineIcon />
+                <span>Add</span>
+            </button>
+
+        </div>
+        <div className='sidenav__more'>
+            <button className='sidenav__button'>
+                <MenuIcon />
+                <span>More</span>   
+            </button>
+        </div>
+    </div>
+
+    
+  )
+}
+
+export default Sidenav
