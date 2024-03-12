15200732a10ebfe63624f3d6a971698510cbbaeb
Duresa Eshetu <duresaeshetu2001@gmail.com>
2024-03-12 18:50:50 +0300

    exchange

--- none/src/firebase.js
+++ none/src/firebase.js
@@ -1,10 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+
+
 const firebaseConfig = {
   apiKey: "AIzaSyClRe8N9PzwIVQVFAvjoR8hfmvD3ZJBEtE",
   authDomain: "exchange-4e3f1.firebaseapp.com",
