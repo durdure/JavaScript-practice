bae6a0d34b849bff6d82972a0606c80025fd32d2
Duresa Eshetu <duresaeshetu2001@gmail.com>
2024-03-12 13:27:48 +0300

    exchange

--- none/src/authenticate/Login.js
+++ none/src/authenticate/Login.js
@@ -1,75 +1,154 @@
+import { useState } from "react";
 import { signInWithEmailAndPassword } from "firebase/auth";
-import React, { useState } from "react";
 import { auth } from "../../src/firebase";
+import { signInWithPopup, GoogleAuthProvider, FacebookAuthProvider } from "firebase/auth";
 import "./Login.css";
-import { Form } from "react-router-dom";
+import {Link} from 'react-router-dom'
 
 function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [popupStyle, showPopup] = useState("hide");
 
   const handleLogin = () => {
-    signInWithEmailAndPassword(auth, email, password).then((authUser) => {
-      window.location.href = "/home";
-    }).catch((error) => {
-      alert("Incorrect email or password. Please try again.");
-    });
+    signInWithEmailAndPassword(auth, email, password)
+      .then((authUser) => {
+        window.location.href = "/home";
+      })
+      .catch((error) => {
+        alert("Incorrect email or password. Please try again.");
+      });
   };
 
+  const popup = () => {
+    showPopup("login-popup");
+    setInterval(() => showPopup("hide"), 3000);
+  };
+
+  const handleLoginAndPopup = () => {
+    handleLogin();
+    popup();
+  };
+
+  const handleLoginWithGoogle = () => {
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        // The signed-in user info.
+        const user = result.user;
+        window.location.href = "/home";
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.error(errorMessage);
+      });
+  };
+
+  const handleLoginWithFacebook = () => {
+    const provider = new FacebookAuthProvider();
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        // The signed-in user info.
+        const user = result.user;
+        window.location.href = "/home";
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.error(errorMessage);
+      });
+  };
 
   return (
-    <div className="wrapper">
+    <div className="cover">
+      <h1>Login</h1>
 
-      <div className="form-wraper sign-in">
-        <Form action="">
-        <h2 style={
-          {
-            color:"#fff",
-            fontSize:"30px",
-            fontWeight:"bold",
-            textAlign:"center",
-
-          }
-        }>Login</h2>
-        
-        <div className="input-group" style={
-          {
-            margin:"20px 0px",
-            position:"relative",
-            borderBottom:"2px solid #fff",
-          }
-        }>
-        <input
-          onChange={(e) => setEmail(e.target.value)}
-          type="email"
-          required
-        />
-        <label for="" >Email</label>
-        </div>
-        <div className="input-group">
-        <input
-          onChange={(e) => setPassword(e.target.value)}
-          type="password"
-        />
-        <label for="">Password</label>
-        </div>
-         
-
-         <div className="remember">
-          <input type="checkbox" name="" id="" />
-          <label for="">Remember me</label>
-         </div>
-         <button onClick={handleLogin} type="submit" >Log in</button>
-
-         <div className="signUp-link">
-          <p>Don't have an account? <a href="">Sign up</a></p>
-         </div>
-        </Form>
+      <input
+        onChange={(e) => setEmail(e.target.value)}
+        type="email"
+        placeholder="Email"
+        required
+        style={{
+          border: "none",
+          outlineColor: "rgb(32,177,255)",
+          backgroundColor: "rgb(229, 216, 216)",
+          height: "4em",
+          width: "80%",
+          borderRadius: "0.28em",
+          textAlign: "center",
+        }}
+      />
+      <input
+        onChange={(e) => setPassword(e.target.value)}
+        type="password"
+        placeholder="Password"
+        style={{
+          border: "none",
+          outlineColor: "rgb(32,177,255)",
+          backgroundColor: "rgb(229, 216, 216)",
+          height: "4em",
+          width: "80%",
+          borderRadius: "0.28em",
+          textAlign: "center",
+        }}
+      />
+      <div className="login_btn" onClick={handleLoginAndPopup}>
+        Log in
+      </div>
       
-        
-    </div>
+      <p>Don't have an account? <Link to='/'>Sign up</Link></p>
 
+      <div className="text">Or Log in Using </div>
 
+      <div className="alt_login">
+        <div
+          className="facebook"
+          onClick={handleLoginWithFacebook}
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            backgroundColor: "rgb(229, 216, 216)",
+          }}
+        >
+          <img
+            src="https://img.icons8.com/color/48/000000/facebook-new.png"
+            alt="facebook"
+            style={{
+              width: "40px",
+              height: "40px",
+              borderRadius: "50%",
+              padding: "5px",
+              backgroundColor: "white",
+            }}
+          />
+        </div>
+        <div
+          className="google"
+          onClick={handleLoginWithGoogle}
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            backgroundColor: "rgb(229, 216, 216)",
+          }}
+        >
+          <img
+            src="https://img.icons8.com/color/48/000000/google-logo.png"
+            alt="google"
+            style={{
+              width: "40px",
+              height: "40px",
+              borderRadius: "50%",
+              padding: "5px",
+              backgroundColor: "white",
+            }}
+          />
+        </div>
+      </div>
     </div>
   );
 }
--- none/src/authenticate/Register/Signup.js
+++ none/src/authenticate/Register/Signup.js
@@ -4,8 +4,11 @@ import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
   updateProfile,
+  signInWithPopup,
 } from "firebase/auth";
 import { auth } from "../../../src/firebase";
+import { Link } from "react-router-dom";
+import { FacebookAuthProvider, GoogleAuthProvider } from "firebase/auth";
 
 function Signup() {
   const [email, setEmail] = useState("");
@@ -16,46 +19,157 @@ function Signup() {
     event.preventDefault();
     createUserWithEmailAndPassword(auth, email, password)
       .then((authUser) => {
-          updateProfile(auth.currentUser, {
-            displayName: username,
-          }).then(() => {
-            window.location.href = "/login";
-          })
+        updateProfile(auth.currentUser, {
+          displayName: username,
+        }).then(() => {
+          window.location.href = "/login";
+        });
       })
       .catch((err) => {
         alert(err);
       });
   };
-  return (
 
-    <div className="main"> 
-     <div className="signup">
-      <img
-        src="https://www.pngkey.com/png/full/828-8286178_mackeys-work-needs-no-elaborate-presentation-or-distracting.png"
-        alt=""
-      />
+  const handleSignUpWithFacebook = () => {
+    const provider = new FacebookAuthProvider();
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        // The signed-in user info.
+        const user = result.user;
+        window.location.href = "/home"; // Redirect to home page after sign up
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.error(errorMessage);
+      });
+  };
+
+  const handleSignUpWithGoogle = () => {
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        // The signed-in user info.
+        const user = result.user;
+        window.location.href = "/home"; // Redirect to home page after sign up
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.error(errorMessage);
+      });
+  };
+
+  return (
+    <div className="coverS">
+      <h1>Sing Up</h1>
       <input
         onChange={(e) => setEmail(e.target.value)}
         type="email"
         placeholder="Email"
         value={email}
+        style={{
+          border: "none",
+          outlineColor: "rgb(32,177,255)",
+          backgroundColor: "rgb(229, 216, 216)",
+          height: "4em",
+          width: "80%",
+          borderRadius: "0.28em",
+          textAlign: "center",
+        }}
       />
       <input
         onChange={(e) => setUsername(e.target.value)}
-        type="email"
+        type="text"
         placeholder="Username"
+        required
         value={username}
+        style={{
+          border: "none",
+          outlineColor: "rgb(32,177,255)",
+          backgroundColor: "rgb(229, 216, 216)",
+          height: "4em",
+          width: "80%",
+          borderRadius: "0.28em",
+          textAlign: "center",
+        }}
       />
       <input
         onChange={(e) => setPassword(e.target.value)}
         type="password"
         placeholder="Password"
         value={password}
+        required
+        style={{
+          border: "none",
+          outlineColor: "rgb(32,177,255)",
+          backgroundColor: "rgb(229, 216, 216)",
+          height: "4em",
+          width: "80%",
+          borderRadius: "0.28em",
+          textAlign: "center",
+        }}
       />
-      <button onClick={handleSignUp}>Sign up</button>
+      <button className="sign_btn" onClick={handleSignUp}
+       style={{
+        border: "none",
+        borderRadius: "0.25em",
+        fontSize:"1em",
+       }}
+      >Sign up</button>
+      <p>
+        Have an account? <Link to="/login">Login</Link>
+      </p>
+
+      <div className="alt_login">
+        <div
+          className="facebook"
+          onClick={handleSignUpWithFacebook}
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            backgroundColor: "rgb(229, 216, 216)",
+          }}
+        >
+          <img
+            src="https://img.icons8.com/color/48/000000/facebook-new.png"
+            alt="facebook"
+            style={{
+              width: "40px",
+              height: "40px",
+              borderRadius: "50%",
+              padding: "5px",
+              backgroundColor: "white",
+            }}
+          />
+        </div>
+        <div
+          className="google"
+          onClick={handleSignUpWithGoogle}
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            backgroundColor: "rgb(229, 216, 216)",
+          }}
+        >
+          <img
+            src="https://img.icons8.com/color/48/000000/google-logo.png"
+            alt="google"
+            style={{
+              width: "40px",
+              height: "40px",
+              borderRadius: "50%",
+              padding: "5px",
+              backgroundColor: "white",
+            }}
+          />
+        </div>
+      </div>
     </div>
-    </div>
-   
   );
 }
 
